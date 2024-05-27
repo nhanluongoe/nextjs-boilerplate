03cb808425c63fa7d237ff6e33f62886de9607d6
nhanluongoe <nhanlt18@gmail.com>
2024-05-27 09:47:36 +0700

    chore(seo): add robots and sitemap

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from 'next';
+import { getBaseUrl } from '@/utils/helpers';
+
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${getBaseUrl()}/sitemap.xml`,
+  };
+}
---
+++ src/app/sitemap.ts
@@ -0,0 +1,15 @@
+import { getBaseUrl } from '@/utils/helpers';
+import type { MetadataRoute } from 'next';
+
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${getBaseUrl()}/`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.7,
+    },
+    // Add more URLs here
+  ];
+}
